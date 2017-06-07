73e19a9ae8a1f22b5a534217d0903eef769e9a95
HenryDev <onyxorca@gmail.com>
2017-06-06 22:18:59 -0500

    update for lidar and radar

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -32,6 +32,15 @@ void KalmanFilter::Update(const VectorXd &z) {
     TODO:
       * update the state by using Kalman Filter equations
     */
+    VectorXd y = z - H_ * x_;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd K = P_ * Ht * Si;
+
+    x_ = x_ + (K * y);
+    MatrixXd I = MatrixXd::Identity(2, 2);
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +48,31 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
     TODO:
       * update the state by using Extended Kalman Filter equations
     */
+    const double &px = x_(0);
+    const double &py = x_(1);
+    const double &vx = x_(2);
+    const double &vy = x_(3);
+
+    double rho = sqrt(px * px + py * py);
+    double theta = atan2(py, px);
+    double rho_dot = (px * vx + py * vy) / rho;
+
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd K = P_ * Ht * Si;
+
+    VectorXd z_pred = VectorXd(3);
+    z_pred << rho, theta, rho_dot;
+
+    VectorXd y = z - z_pred;
+    x_ = x_ + (K * y);
+    MatrixXd I = MatrixXd::Identity(2, 2);
+    P_ = (I - K * H_) * P_;
+
+    VectorXd u = VectorXd(2);
+    u << 0, 0;
+    x_ = F_ * x_ + u;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft + Q_;
 }
